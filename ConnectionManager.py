0fa16337457b7d9426e8bce57c350cb9266a4977
weixiangtoh <weixiangtoh982@gmail.com>
2020-05-09 16:45:23 +0800

    Added UserDAO

--- ConnectionManager.py
+++ ConnectionManager.py
@@ -7,11 +7,4 @@ mydb = mysql.connector.connect(
   database="covid"
 )
 
-# mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
 mycursor = mydb.cursor()
-
-mycursor.execute("SHOW TABLES")
-# mycursor.execute("CREATE TABLE post (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), request VARCHAR(255), location VARCHAR(255), status BOOLEAN)")
-
-for x in mycursor:
-  print(x)
--- db.sql
+++ db.sql
@@ -10,16 +10,3 @@ CREATE TABLE IF NOT EXISTS `POST`(
   `location` varchar(50) NOT NULL,
   `status` BOOLEAN NOT NULL  
 ) 
-
---
--- password decode for each of the users:
--- alice1
--- bobby2
--- charlie3
--- daryl4
---
-
--- INSERT INTO `USER` VALUES ('alice', 'Alice NG', '$2y$10$nNEB5sQQWA9dPEovaYEcpOt3cVFpWRZN4jmMNqgvXU1cWu7TiZCkS');
--- INSERT INTO `USER` VALUES ('bobby', 'Bobby TAN', '$2y$10$1SxSjTk/mNL6ETuZBDDzJ..iWzow.bISd2cGiz6q0tG6CgTYTchHe');
--- INSERT INTO `USER` VALUES ('charlie', 'Charlie LEE', '$2y$10$v8IZ63lH3u9DfR2kg8A/oefyQIEB45x2lWBji4rBCAMBiXtPhLaDS');
--- INSERT INTO `USER` VALUES ('daryl', 'Daryl KOH', '$2y$10$/44hgwbCUNG0wUcpDH1bnek36YbLmbG0VAja5kZig2Sax/8xfnpO6');
